a64e18ade9c4bca7d01f31a286484e4040c8059c
Nathan Pearl <nathanpearl2001@gmail.com>
2020-04-25 00:36:22 -0400

    Add project files.

---
+++ CapstoneProject/Program.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneProject
+{
+    // **************************************************
+    //
+    // Title: CapStoneProject 
+    // Description: This is an application that makes trivia questions into a game programm.
+    // Application Type: Console
+    // Author: Pearl, Natham
+    // Dated Created: 4/24/2020
+    // Last Modified: 4/25/2020
+    //
+    // **************************************************
+
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            SetTheme();
+
+            DisplayWelcomeScreen();
+            DisplayMenuScreen();
+            DisplayClosingScreen();
+        }
+
+        /// <summary>
+        /// setup the console theme
+        /// </summary>
+        static void SetTheme()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// *****************************************************************
+        /// *                     Main Menu                                 *
+        /// *****************************************************************
+        /// </summary>
+        static void DisplayMenuScreen()
+        {
+            Console.CursorVisible = true;
+
+            bool quitApplication = false;
+            string menuChoice;
+            do
+            {
+                DisplayScreenHeader("Main Screen");
+                Console.WriteLine("\ta) Trivia Game ");
+                Console.WriteLine("\tb) ");
+                Console.WriteLine("\tc) ");
+                Console.WriteLine("\td) ");
+                Console.WriteLine("\te) ");
+                Console.WriteLine("\tf) ");
+                Console.WriteLine("\tq) Quit");
+                Console.Write("\t\tEnter Choice:");
+                menuChoice = Console.ReadLine().ToLower();
+
+                switch (menuChoice)
+                {
+                    case "a":
+                        DisplayGameMenuScreen();
+                        break;
+
+                    case "b":
+
+                        break;
+
+                    case "c":
+
+                        break;
+
+                    case "d":
+
+                        break;
+
+                    case "e":
+
+                        break;
+
+                    case "f":
+
+                        break;
+
+                    case "q":
+
+                        quitApplication = true;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("\tPlease enter a letter for the menu choice.");
+                        DisplayContinuePrompt();
+                        break;
+                }
+
+            } while (!quitApplication);
+        }
+
+        #region trivia Game
+
+        /// <summary>
+        /// *****************************************************************
+        /// *                     Talent Show Menu                          *
+        /// *****************************************************************
+        /// </summary>
+        /// 
+
+
+        static void DisplayGameMenuScreen()
+        {
+            Console.CursorVisible = true;
+
+            bool quitGameCatacoriesMenu = false;
+            string menuChoice;
+
+            do
+            {
+                DisplayScreenHeader("\tTrivia Time");
+
+                //
+                // get user menu choice
+                //
+                Console.WriteLine("\ta) Music");
+                Console.WriteLine("\tb) Sports Legends");
+                Console.WriteLine("\tc) Fun Facts");
+                Console.WriteLine("\tq) Main Menu");
+                Console.Write("\t\tEnter Choice:");
+                menuChoice = Console.ReadLine().ToLower();
+
+                //
+                // process user menu choice
+                //
+                switch (menuChoice)
+                {
+                    case "a":
+                        DisplayMusic();
+                        break;
+
+                    case "b":
+                        DisplaySportsLegends();
+                        break;
+
+                    case "c":
+                        DisplayFunFacts();
+                        break;
+
+                    case "q":
+                        quitGameCatacoriesMenu = true;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("\tPlease enter a letter for the menu choice.");
+                        DisplayContinuePrompt();
+                        break;
+                }
+
+            } while (!quitGameCatacoriesMenu);
+        }
+
+        static void DisplayFunFacts()
+        {
+            
+
+
+
+        }
+
+        static void DisplaySportsLegends()
+        {
+           
+            
+
+
+
+        }
+
+        static void DisplayMusic()
+        {
+            
+
+
+
+
+        }
+
+
+
+
+
+        #endregion
+
+        #region USER INTERFACE
+
+        /// <summary>
+        /// *****************************************************************
+        /// *                     Welcome Screen                            *
+        /// *****************************************************************
+        /// </summary>
+        static void DisplayWelcomeScreen()
+        {
+            Console.CursorVisible = false;
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\tJeperdy");
+            Console.WriteLine();
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// *****************************************************************
+        /// *                     Closing Screen                            *
+        /// *****************************************************************
+        /// </summary>
+        static void DisplayClosingScreen()
+        {
+            Console.CursorVisible = false;
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\tThank you for using Jeperdy show!");
+            Console.WriteLine();
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// display continue prompt
+        /// </summary>
+        static void DisplayContinuePrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\tPress any key to continue.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// display menu prompt
+        /// </summary>
+        static void DisplayMenuPrompt(string menuName)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"\tPress any key to return to the {menuName} Menu.");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// display screen header
+        /// </summary>
+        static void DisplayScreenHeader(string headerText)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\t" + headerText);
+            Console.WriteLine();
+        }
+        #endregion
+    }
+}
